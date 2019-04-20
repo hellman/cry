ef02e00f97668ecb1e35d3a2c5ad365ac1b712e6
hellman <hellman@users.noreply.github.com>
2019-04-20 13:21:34 +0200

    marshalled cache of iterables (e.g. for pypy)

--- py/utils/cache.py
+++ py/utils/cache.py
@@ -1,7 +1,17 @@
-import ast, pickle
+#-*- coding:utf-8 -*-
+
+import sys
+import ast, pickle, marshal
+import random
 from functools import wraps
 import hashlib
 H = lambda s: hashlib.sha256(s).hexdigest()
+
+def truncrepr(a, limit=128):
+    s = repr(a)
+    if len(s) > limit:
+        s = s[:limit] + "..."
+    return s
 
 def ast_cache(filename_prefix):
     def deco(func):
@@ -27,8 +37,8 @@ def ast_cache(filename_prefix):
                 res = func(*a, **k)
                 f.write("Cache: %s(%s; %s)\n" % (
                     func.__name__,
-                    ", ".join(map(repr, a)),
-                    ", ".join("%s=%r" % item for item in sorted(k.items()))
+                    ", ".join(map(truncrepr, a)),
+                    ", ".join("%s=%s" % (kk, truncrepr(vv)) for kk, vv in sorted(k.items()))
                 ))
                 assert ast.literal_eval(repr(res)) == res, "non-serializable res"
                 f.write(repr(res) + "\n")
@@ -56,10 +66,63 @@ def pypickle_cache(filename_prefix):
                 res = func(*a, **k)
                 f.write("Cache: %s(%s; %s)\n" % (
                     func.__name__,
-                    ", ".join(map(repr, a)),
-                    ", ".join("%s=%r" % item for item in sorted(k.items()))
+                    ", ".join(map(truncrepr, a)),
+                    ", ".join("%s=%s" % (kk, truncrepr(vv)) for kk, vv in sorted(k.items()))
                 ))
                 pickle.dump(res, f)
             return res
         return wrapper
     return deco
+
+
+
+def marshal_cache_iter(filename_prefix_or_func):
+    if isinstance(filename_prefix_or_func, str):
+        filename_prefix = filename_prefix_or_func
+    else:
+        filename_prefix = filename_prefix_or_func.__name__
+
+    def deco(func):
+        @wraps(func)
+        def wrapper(*a, **k):
+            cache_key = tuple(a) + tuple(sorted(k.items()))
+            cache_key = H(str(cache_key))
+
+            filename = filename_prefix + "." + cache_key
+            try:
+                f = open(filename)
+                info = f.readline()
+                assert info.startswith("Cache: ")
+                token = marshal.load(f)
+                while True:
+                    obj = marshal.load(f)
+                    if obj == token:
+                        print >>sys.stderr, "Success loading cache:", func.__name__, filename
+                        return
+                    yield obj
+            except KeyboardInterrupt:
+                print >>sys.stderr, "KB received, raising..."
+                raise
+            except Exception as err:
+                print >>sys.stderr, "Error loading cache:", func.__name__, filename, ":", err
+
+            token = "%064x" % random.randrange(2**256)
+            with open(filename, "w") as f:
+                f.write("Cache: %s(%s; %s)\n" % (
+                    func.__name__,
+                    ", ".join(map(truncrepr, a)),
+                    ", ".join("%s=%s" % (kk, truncrepr(vv)) for kk, vv in sorted(k.items()))
+                ))
+                f.flush()
+                marshal.dump(token, f)
+                for res in func(*a, **k):
+                    marshal.dump(res, f)
+                    yield res
+                marshal.dump(token, f)
+            return
+        return wrapper
+
+    if isinstance(filename_prefix_or_func, str):
+        return deco
+    else:
+        return deco(filename_prefix_or_func)
